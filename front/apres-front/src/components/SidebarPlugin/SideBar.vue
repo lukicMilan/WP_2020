efc6c4e6c99323c20a3bd2b4d733614fb4876068
lukicMilan <lukic.milan22@gmail.com>
2020-09-03 14:00:18 +0200

    initial front page

---
+++ front/apres-front/src/components/SidebarPlugin/SideBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <md-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="showNavigation = true">
+            <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">My Title</span>
+        </md-toolbar>
+        <md-drawer :md-active.sync="showNavigation" md-swipeable>
+        <md-toolbar class="md-transparent" md-elevation="0">
+            <span class="md-title">My App name</span>
+        </md-toolbar>
+
+        <md-list>
+            <md-list-item  to="/hello">
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+            </md-list-item>
+
+            <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+            </md-list-item>
+
+            <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+            </md-list-item>
+
+            <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+            </md-list-item>
+        </md-list>
+        </md-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "side-bar",
+    data: () => ({
+      showNavigation: false,
+      showSidepanel: false
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+  .page-container {
+    min-height: 300px;
+    overflow: hidden;
+    position: relative;
+    border: 1px solid rgba(#000, .12);
+  }
+
+   // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+
+  .md-content {
+    padding: 16px;
+  }
+  
+</style>
