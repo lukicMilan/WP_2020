62e4361fedcb507bc31fd6529f6b779c4dc56707
abuhmiler <abuhmiler4@gmail.com>
2020-09-09 21:22:29 +0200

    Access denied page added.

---
+++ front/apres-front/src/pages/AccessDenied.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class = "accessDenied" align = center>
+        <div>
+        <span class="md-display-4">Access Denied</span>
+        </div>
+        <div>
+        <span class="md-display-1">You do not have access to this page.</span>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'AccessDenied'
+}
+</script>
+
+<style lang="scss" scoped>
+    .center {
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+    }
+</style>
